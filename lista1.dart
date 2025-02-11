f211be6c7ede487be440fe7160cd38940bd4c0c3
kaylee luevano molina <a.22308051281248@cbtis128.edu.mx>
2025-02-11 16:59:45 +0000

    resultado

--- lista1.dart
+++ lista1.dart
@@ -1,5 +1,6 @@
 void main(){
 // lista tipo entero de 3 elementos
+print("Kaylee Alejandra Luevano Molina mat: 22308051281248 gpo 6-J");
  List<int> numeros = [10, 20, 30];
  print("Lista de numeros enteros: ");
  print(numeros);
